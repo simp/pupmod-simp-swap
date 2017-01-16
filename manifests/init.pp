7f7227afe7eb339dc6dc5d3059e9818bde26c3af
jeannegreulich <jeanne.greulich@onyxpoint.com>
2017-01-16 14:27:51 -0500

    (SIMP-2462) Update Versions (#2)
    Updated the requires and metadata.json files
    
    SIMP-2462 #comment swap updated

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,39 +6,39 @@
 #
 # An absolute value setting will always override the cron job.
 #
-# @params dynamic_script Boolean
+# @params dynamic_script
 #   If true, place the dynamic_swappiness script on the system and enable it. Set
 #   this to false if you'd like to manage the swappiness of the system manually
 #   with $swappiness.
-# @params swappiness Integer
+# @params swappiness
 #   If dynamic_script is set to false, set the system to run at this swappiness
 #   and do not adjust. Take care, whatever value you place in here used as-is!
 #
-# @params cron_step Integer[0,59]
+# @params cron_step
 #   The crontab(5) minute step value for the swappiness set.
-# @params maximum Integer[0,100]
+# @params maximum
 #   The percentage of memory free on the system above which we will set
 #   vm.swappiness to $min_swappiness
-# @params median Integer[0,100]
+# @params median
 #   If the percentage of free memory on the system is between this number and
 #   $maximum, set vm.swappiness to $low_swappiness.
-# @params minimum Integer[0,100]
+# @params minimum
 #   If the percentage of free memory on the system is between this number and
 #   $median, set vm.swappiness to $high_swappiness.  If below this number,
 #   set to $max_swappiness.
-# @params min_swappiness Integer[0,100]
+# @params min_swappiness
 #   The minimum swappiness to ever set on the system.
-# @params low_swappiness Integer[0,100]
+# @params low_swappiness
 #   The next level of swappiness to jump to on the system.
-# @params high_swappiness Integer[0,100]
+# @params high_swappiness
 #   The medium-high level of swappiness to set on the sysetm.
-# @params max_swappiness Integer[0,100]
+# @params max_swappiness
 #   The absolute maximum to ever set the swappiness on the system.
 #
 class swap (
-  Boolean $dynamic_script         = true,
-  Integer $swappiness             = 0,
-  Integer[0,59] $cron_step        = 5,
+  Boolean        $dynamic_script  = true,
+  Integer        $swappiness      = 0,
+  Integer[0,59]  $cron_step       = 5,
   Integer[0,100] $maximum         = 30,
   Integer[0,100] $median          = 10,
   Integer[0,100] $minimum         = 5,
