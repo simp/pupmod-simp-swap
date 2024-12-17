aedd103645155128b654013dc4b4813aaf8e5c6d
Steven Pritchard <steve@sicura.us>
2024-12-17 09:27:04 -0600

    Clean up for rubocop

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -3,26 +3,26 @@ require 'spec_helper_acceptance'
 test_name 'swap class'
 
 describe 'swap class' do
-  let(:manifest) {
+  let(:manifest) do
     <<-EOS
       include 'swap'
     EOS
-  }
+  end
 
   hosts.each do |host|
     context 'default parameters' do
-      it 'should work with no errors' do
-        apply_manifest_on(host, manifest, :catch_failures => true)
+      it 'works with no errors' do
+        apply_manifest_on(host, manifest, catch_failures: true)
       end
 
-      it 'should be idempotent' do
-        apply_manifest_on(host, manifest, :catch_changes => true)
+      it 'is idempotent' do
+        apply_manifest_on(host, manifest, catch_changes: true)
       end
 
-      it 'should set the system swappiness to 60' do
+      it 'sets the system swappiness to 60' do
         on(host, 'sysctl vm.swappiness=20')
 
-        apply_manifest_on(host, manifest, :catch_failures => true)
+        apply_manifest_on(host, manifest, catch_failures: true)
 
         result = on(host, 'sysctl -n vm.swappiness').stdout.strip.to_i
 
